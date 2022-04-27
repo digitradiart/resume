ce01880c417099248b0cec4d222c96af83168cf4
digitradiart <digitradiart@gmail.com>
2022-04-27 11:59:42 +0700

    fix header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,10 +8,12 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Ayu Agustina</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
-        <CTA />
+        <div className="center-header">
+          <h5 className="center-header-child">Hello I'm</h5>
+          <h1 className="center-header-child">Ayu Agustina</h1>
+          <h5 className="center-header-child">Fullstack Developer</h5>
+          <CTA className="center-header-child" />
+        </div>
 
         <div className="scroll-down">
           <a href="#about">Scroll Down</a>
