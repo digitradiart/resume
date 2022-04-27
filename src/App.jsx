a6e62e1438887d6159ed25df158bd14d10958a13
digitradiart <digitradiart@gmail.com>
2022-04-27 12:17:15 +0700

    fix header

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
-import About from './components/about/About';
-import Experience from './components/experience/Experience';
-import Portfolio from './components/portfolio/Portfolio';
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
+// import About from './components/about/About';
+// import Experience from './components/experience/Experience';
+// import Portfolio from './components/portfolio/Portfolio';
+// import Contact from './components/contact/Contact';
+// import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
     <>
       <Header />
       <Nav />
-      <About />
+      {/* <About />
       <Experience />
       <Portfolio />
       <Contact />
-      <Footer />
+      <Footer /> */}
     </>
   );
 };
