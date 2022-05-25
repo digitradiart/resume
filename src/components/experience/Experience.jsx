3047a0e756ea03f7d88262f3cfe563728df7a3b2
digitradiart <digitradiart@gmail.com>
2022-05-26 03:40:26 +0700

    fix experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,8 +4,125 @@ import "./experience.css";
 const Experience = () => {
   return (
     <section id="experience">
-      <h5>What Skills I Have</h5>
+      <h5>What I've been Learning</h5>
       <h2>My Experiences</h2>
+
+      <div className="container experience__container">
+        <div className="experience_content">
+          <div className="experience_cards">
+            <article className="experience_card">
+              <img
+                src="https://img.icons8.com/windows/60/ffffff/parse-resumes.png"
+                alt="experience"
+              />
+              <h5>Web Development</h5>
+              <small>
+                <ul>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                </ul>
+              </small>
+            </article>
+
+            <article className="experience_card">
+              <img
+                src="https://img.icons8.com/windows/60/ffffff/parse-resumes.png"
+                alt="experience"
+              />
+              <h5>Data Science</h5>
+              <small>
+                <ul>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                </ul>
+              </small>
+            </article>
+
+            <article className="experience_card">
+              <img
+                src="https://img.icons8.com/windows/60/ffffff/parse-resumes.png"
+                alt="experience"
+              />
+              <h5>Graphic Design</h5>
+              <small>
+                <ul>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                      Front end
+                    </a>
+                  </li>
+                </ul>
+              </small>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
